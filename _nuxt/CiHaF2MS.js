239a55ca49b6da713c804c4379e64a25ea093bef
salehye <147006569+salehye@users.noreply.github.com>
2025-11-03 17:19:27 +0000

    Deploying to gh-pages from @ salehye/canvas@b645ea55548903cf483635da4d8ce7d12d60f4f7 🚀

---
+++ _nuxt/CiHaF2MS.js
@@ -0,0 +1,1 @@
+import{bm as r}from"./GYYTq9UC.js";function s(t,e="reka"){return`${e}-${r?.()}`}export{s as u};
---
+++ _nuxt/Cz-N781u.js
@@ -0,0 +1,1 @@
+import{r as n}from"./C5mYKyr6.js";import a from"./DknlmSth.js";import{x as s,G as t,d as e,w as i,E as p}from"./GYYTq9UC.js";import"./iik6CYzq.js";const d={__name:"ProseWarning",setup(c){const o=s();return(r,f)=>(e(),t(a,{color:"warning",icon:p(o).ui.icons.warning},{default:i(()=>[n(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
